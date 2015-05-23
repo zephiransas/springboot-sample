543ee3e6f9c378f27dc9750cc96eb9df8b6e74b8
zephiransas <zephiransas@gmail.com>
2015-05-23 15:15:08 +0900

    Prepare for deploy to Heroku

--- src/main/java/springboot/AppConfig.java
+++ src/main/java/springboot/AppConfig.java
@@ -1,19 +1,46 @@
 package springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @Configuration
 public class AppConfig {
 
+	@Autowired
+	DataSourceProperties properties;
+
 	@Bean
 	@ConfigurationProperties("spring.datasource")
-	public DataSource dataSource() {
-		return DataSourceBuilder.create().build();
+	public DataSource dataSource() throws URISyntaxException {
+		String url;
+		String username;
+		String password;
+
+		String databseUrl = System.getenv("DATABASE_URL");
+		if(databseUrl == null) {
+			url = properties.getUrl();
+			username = properties.getUsername();
+			password = properties.getPassword();
+		} else {
+			URI dbUri = new URI(databseUrl);
+			url = "jdbc:postgresql://" + dbUri.getHost() + ":" + dbUri.getPort() + dbUri.getPath();
+			username = dbUri.getUserInfo().split(":")[0];
+			password = dbUri.getUserInfo().split(":")[1];
+		}
+
+		return DataSourceBuilder.create(this.properties.getClassLoader())
+			.url(url)
+			.username(username)
+			.password(password)
+			.build();
 	}
 
 }
